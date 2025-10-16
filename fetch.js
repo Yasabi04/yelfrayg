002a43836c15d20a0c1514272b9d91a3916f1e98
Yassin El Fraygui <yassin.el_fraygui@smail.th-koeln.de>
2025-10-16 22:44:07 +0200

    update: Neues Design für Karten

--- fetch.js
+++ fetch.js
@@ -1,36 +1,46 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const projectContainer = document.querySelector(".project-container");
+// document.addEventListener("DOMContentLoaded", () => {
+//     const projectContainer = document.querySelector(".project-container");
 
-    fetch("./projects.json")
-        .then((response) => response.json())
-        .then((data) => {
-            const collectProjects = data.projects;
+//     fetch("./projects.json")
+//         .then((response) => response.json())
+//         .then((data) => {
+//             const collectProjects = data.projects;
 
-            collectProjects.forEach((project) => {
-                const isYearNumber = !isNaN(project.year);
-                const projectElement = document.createElement("a");
-                projectElement.href = project.link;
-                projectElement.classList.add("single-project");
-                projectElement.innerHTML = `
-                    <h3 class="project-title">${project.title}</h3>
-                    ${
-                        isYearNumber
-                            ? `<div class="project-year">${project.year}</div>`
-                            : `<div class="project-year in-dev">${project.year}</div>`
-                    }
-                    <div class="project-information">
-                        <p class="project-description">${
-                            project.description
-                        }</p>
-                        <ul class="tech-stack">
-                            ${project.technologies
-                                .map((tech) => `<li class="tech">${tech}</li>`)
-                                .join("")}
-                        </ul>
-                    </div>
-                `;
-                projectContainer.appendChild(projectElement);
-            });
+//             collectProjects.forEach((project) => {
+//                 const isYearNumber = !isNaN(project.year);
+//                 const projectElement = document.createElement("a");
+//                 projectElement.href = project.link;
+//                 projectElement.classList.add("single-project");
+//                 projectElement.innerHTML = `
+//                     <h3 class="project-title">${project.title}</h3>
+//                     ${
+//                         isYearNumber
+//                             ? `<div class="project-year">${project.year}</div>`
+//                             : `<div class="project-year in-dev">${project.year}</div>`
+//                     }
+//                     <div class="project-information">
+//                         <p class="project-description">${
+//                             project.description
+//                         }</p>
+//                         <ul class="tech-stack">
+//                             ${project.technologies
+//                                 .map((tech) => `<li class="tech">${tech}</li>`)
+//                                 .join("")}
+//                         </ul>
+//                     </div>
+//                 `;
+//                 projectContainer.appendChild(projectElement);
+//             });
+//         })
+//         .catch((error) => console.error("Error fetching projects:", error));
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const allProjects = document.querySelectorAll(".project")
+
+    allProjects.forEach(project => {
+        project.addEventListener('click', () => {
+            project.classList.toggle('full')
         })
-        .catch((error) => console.error("Error fetching projects:", error));
-});
+    });
+})
